34901318e92b8572aa2abcb262a128bdf7827830
kuuro011 <ghiewinantonio011@gmail.com>
2024-01-11 21:27:52 +0800

    nothing new

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    private Animator Animator;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trao"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Animator.SetTrigger("Death");
+    }
+}
