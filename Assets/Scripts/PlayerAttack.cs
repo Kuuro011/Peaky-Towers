72a3a1985b6aacd18275a037463d848f26ef4386
kuuro011 <ghiewinantonio011@gmail.com>
2024-01-16 22:36:02 +0800

    Atk animations doen

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -8,7 +8,8 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private Transform attackPos;
     [SerializeField] private float attackRange;
     [SerializeField] private LayerMask EnemyLayer;
-
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask Ground;
     [SerializeField] private int atkDamage;
     void Start()
     {
@@ -17,15 +18,29 @@ public class PlayerAttack : MonoBehaviour
     
    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (IsGrounded())
         {
-            Attack();
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Attack_1();
+            }
+
+            else if (Input.GetKeyDown(KeyCode.X))
+            {
+                Attack_2();
+            }
+            
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                Attack_3();
+            }
         }
+        
     }
 
-    void Attack()
+    void Attack_1()
     {
-        animator.SetTrigger("Player_Attack");
+        animator.SetTrigger("Atk_1");
 
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, EnemyLayer);
 
@@ -34,4 +49,34 @@ public class PlayerAttack : MonoBehaviour
             enemies.GetComponent<Enemy>().TakeDamage(atkDamage);
         }
     }
+
+    void Attack_2()
+    {
+        animator.SetTrigger("Atk_2");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, EnemyLayer);
+
+        foreach (Collider2D enemies in hitEnemies)
+        {
+            enemies.GetComponent<Enemy>().TakeDamage(atkDamage);
+        }
+    }
+
+    void Attack_3()
+    {
+        animator.SetTrigger("Atk_3");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPos.position, attackRange, EnemyLayer);
+
+        foreach (Collider2D enemies in hitEnemies)
+        {
+            enemies.GetComponent<Enemy>().TakeDamage(atkDamage);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, Ground);
+    }
+
 }
